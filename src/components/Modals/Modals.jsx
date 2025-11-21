193ea29c3044b590180de1ffd9b61eed39ab8bc9
Fernanda Borges <fernandaborges133@gmail.com>
2025-11-21 14:37:48 -0300

    remocao de itens

--- src/components/Modals/Modals.jsx
+++ src/components/Modals/Modals.jsx
@@ -1,13 +1,8 @@
 
-import React from 'react';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
 import Modal from 'react-bootstrap/Modal';
 import Row from 'react-bootstrap/Row';
-// import Button from 'react-bootstrap/Button';
 import QrCode from '../../assets/img/Code.jpg'
 import Ancora from "../../components/Button/Ancora.jsx";
-import GridLayout from '../GridLayout/GridLayout.jsx';
 import Button from '../Button/Button.jsx';
 
 const Modals = ({show, handleClose}) => {
@@ -17,14 +12,14 @@ const Modals = ({show, handleClose}) => {
         <Modal.Header >
           <Modal.Title className='fw-bold'>Informações de contato</Modal.Title>
         </Modal.Header>
-        <Modal.Body >
+        <Modal.Body>
             <Row> 
                <p><span>Email: </span><Ancora onLink="mailto:fernandaborgesb4@gmail.com">fernandaborgesb4@gmail.com</Ancora> </p>
                
             </Row>
             <Row>
-                <div className='d-flex'>
-                    <div><span>WhatsApp:</span><br/>(61) 9 96294438</div>
+                <div className='d-flex flex-column'>
+                    <div><span>WhatsApp:</span></div>
                     <div>
                         <img src={QrCode} alt="" style={{width:"300px"}} />
                         <p class="text-center"><span>Escaneie-me</span></p>
@@ -33,9 +28,6 @@ const Modals = ({show, handleClose}) => {
             </Row>
         </Modal.Body>
         <Modal.Footer>
-          {/* <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button> */}
           <Button size="btn-contato" onClick={handleClose}>Fechar</Button>
         </Modal.Footer>
       </Modal>
